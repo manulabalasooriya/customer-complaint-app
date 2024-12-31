6768d711772ad611fc637d2ce9f2dd76065dc8cb
Manula Balasooriya <manulabalasooriya@MacBookAir.lan>
2024-12-31 09:28:50 +0530

    4th

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,12 +1,15 @@
+var bodyParser = require('body-parser')
+var urlEncodedParser = bodyParser.urlencoded({extended:'false'})
+
 module.exports = (app) => {
 
-
+   data = ['A','B','C']
 
     app.get('/complaint',(req,res) => {
-        res.render('complaints', { data: { data: ['Complaint 10', 'Complaint 2'] } });
+        res.render('complaints', {data:data})
     })
-    app.post('/complaint',(req,res) => {
-        
+    app.post('/complaint',urlEncodedParser,(req,res) => {
+        console.log(req.url)
     })
     app.delete('/complaint',(req,res) => {
         
